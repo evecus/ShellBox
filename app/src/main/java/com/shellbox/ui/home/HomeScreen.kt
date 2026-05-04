7e73b4718f39099c2b76d8c3d7f415ce0feaab0c
evecus <jamie55@163.com>
2026-05-04 19:56:32 +0800

    Add files via upload

--- app/src/main/java/com/shellbox/ui/home/HomeScreen.kt
+++ app/src/main/java/com/shellbox/ui/home/HomeScreen.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
