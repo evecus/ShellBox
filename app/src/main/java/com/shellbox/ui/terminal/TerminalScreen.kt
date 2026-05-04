becc365b94ad7a69dd91227126a5100113f4f5ad
evecus <jamie55@163.com>
2026-05-04 22:15:15 +0800

    Update TerminalScreen.kt

--- app/src/main/java/com/shellbox/ui/terminal/TerminalScreen.kt
+++ app/src/main/java/com/shellbox/ui/terminal/TerminalScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.material.icons.outlined.*
